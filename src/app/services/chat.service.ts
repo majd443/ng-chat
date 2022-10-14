8a140d0742e546b7379c04ee75fdc5bc9cdce35f
majd443 <m.mazarib@hotmail.com>
2022-10-14 16:13:23 +0300

    added design for chat container, added chat service methods

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { IChatRoom } from '../models';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
-
-  constructor() { }
+  constructor(private _db: AngularFirestore) {}
 
   /**
    * get rooms
@@ -13,6 +15,18 @@ export class ChatService {
    * get room messages
    * send message
    * delete room
-   * 
    */
+
+  public getRooms(): Observable<Array<IChatRoom>> {
+    return this._db.collection('rooms').snapshotChanges().pipe(map(snaps => {
+      return snaps.map(snap => {
+        const id = snap.payload.doc.id;
+        const data: IChatRoom = <IChatRoom> snap.payload.doc.data();
+        return <IChatRoom> {
+          ...data,
+          id,
+        }
+      })
+    }))
+  }
 }
