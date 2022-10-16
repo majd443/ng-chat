2b88f5373c7816de1f73712c187453051ffb9afe
majd443 <m.mazarib@hotmail.com>
2022-10-16 16:10:34 +0300

    added send message functionality

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { IMessage } from 'src/app/models';
 
 @Component({
@@ -7,22 +15,26 @@ import { IMessage } from 'src/app/models';
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+  @ViewChild('virtualScroll') virtualScroll?: CdkVirtualScrollViewport;
   @Output() onSendMessage: EventEmitter<string> = new EventEmitter();
 
   @Input() set messages(messages: Array<IMessage>) {
     this._messages = messages.sort((x, y) => {
-      return x.timeStamp - y.timeStamp
-    })
-  };
+      return x.timeStamp - y.timeStamp;
+    });
+    this.virtualScroll?.scrollToIndex(this._messages.length - 1)
+  }
 
   get messages() {
     return this._messages;
   }
 
-  private _messages: Array<IMessage> = []
+  private _messages: Array<IMessage> = [];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+   
+  }
 
   public sendMessage(message: string, input: HTMLInputElement): void {
     this.onSendMessage.emit(message);
